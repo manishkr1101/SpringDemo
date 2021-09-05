cb03c92d484ce9de21342ee46c6c72a08a8bcd2f
Manish Kumar <manishkumarsh120@gmail.com>
2021-09-06 03:11:10 +0530

    Used lambok to reduce boiler code from Entity class

--- src/main/java/com/manish/springdemo/entity/Department.java
+++ src/main/java/com/manish/springdemo/entity/Department.java
@@ -1,5 +1,7 @@
 package com.manish.springdemo.entity;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,6 +9,10 @@ import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -27,55 +33,5 @@ public class Department {
     private String address;
     private String deptCode;
 
-    public Department(Long id, String name, String address, String deptCode) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.deptCode = deptCode;
-    }
 
-    public Department() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getDeptCode() {
-        return deptCode;
-    }
-
-    public void setDeptCode(String deptCode) {
-        this.deptCode = deptCode;
-    }
-
-    @Override
-    public String toString() {
-        return "Department{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", address='" + address + '\'' +
-                ", deptCode='" + deptCode + '\'' +
-                '}';
-    }
 }
