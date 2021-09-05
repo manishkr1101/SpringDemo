fe76bc4e012d4e44cb091e40a5871806342375a2
Manish Kumar <manishkumarsh120@gmail.com>
2021-09-05 23:30:17 +0530

    initial commit

---
+++ src/main/java/com/manish/springdemo/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.manish.springdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String sayHello() {
+        return "hello jkk nj";
+    }
+}
